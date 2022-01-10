7d9f2a36d40644a7e1de8b43ecb7905993ff77d2
mateus <mateusqueiroz011@gmail.com>
2022-01-09 21:10:38 -0300

    adicionado o codigo swicth

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Atv_swicth_break
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            int tempo=0;
+            char escolha;
+
+
+            Console.WriteLine("Fortaleza/CE a Rio de janeiro/ RJ");
+            Console.WriteLine("Escolha o transporte : [a] Aviao | [c] carro | [o] onibus ");
+
+
+            escolha = char.Parse(Console.ReadLine());
+
+
+
+            switch (escolha)
+            {
+                case 'a':
+                    tempo = 50;
+                    break;
+
+                case 'c':
+                    tempo = 480;
+                    break;
+
+                case 'o':
+                    tempo = 550;
+                    break;
+
+                default:
+                    tempo = -1;
+                    break;
+            }
+            
+                    if(tempo < 0)
+            {
+                Console.WriteLine("transporte indisponivel");
+
+            }
+            else
+            {
+                Console.WriteLine("o tempo para o transporte escolhido é: "  + tempo + " minutos " );
+            }
+
+          
+                 
+
+
+
+
+
+        
+        
+        
+        
+        }
+    }
+}
